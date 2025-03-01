bb48af77fceed0f127ee1c6bcb461f1ac2d3cbe5
gustavoalara <gustavoa.laraheredero@gmail.com>
2025-03-01 16:51:38 +0100

    Added new Highscore function

--- DOF2DMD/Program.cs
+++ DOF2DMD/Program.cs
@@ -323,7 +323,7 @@ namespace DOF2DMD
             public static ushort dmdWidth => ushort.Parse(_configuration["dmd_width"] ?? "128");
             public static ushort dmdHeight => ushort.Parse(_configuration["dmd_height"] ?? "32");
             public static string StartPicture => _configuration["start_picture"] ?? "DOF2DMD";
-            public static bool hi2txt_enabled => _configuration["hi2txt_enabled"] ?? "false";
+            public static bool hi2txt_enabled => Boolean.Parse(_configuration["hi2txt_enabled"] ?? "false");
             public static string hi2txt_path => _configuration["hi2txt_path"] ?? "c:\\hi2txt";
             public static string mame_path => _configuration["mame_path"] ?? "c:\\mame";
         }
