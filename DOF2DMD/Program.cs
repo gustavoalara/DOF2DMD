1c62002778048d21c60c06e27c37078faf1e65f7
gustavoalara <gustavoa.laraheredero@gmail.com>
2025-03-01 17:18:04 +0100

    fix an error in hiscore file path

--- DOF2DMD/Program.cs
+++ DOF2DMD/Program.cs
@@ -655,7 +655,7 @@ namespace DOF2DMD
         {
             // Construir la ruta completa del ejecutable y el archivo de highscore
             string hi2txtExe = System.IO.Path.Combine(AppSettings.hi2txt_path, "Hi2Txt.exe");
-            string hiscoreFile = System.IO.Path.Combine(AppSettings.mame_path, "hiscore", $"{game}");
+            string hiscoreFile = System.IO.Path.Combine(AppSettings.mame_path, "hiscore", $"{game}.hi");
             LogIt($"Hi2txt set to {hi2txtExe}");
             LogIt($"Game highscore file set to {hiscoreFile}");
             // Verificar si los archivos existen antes de ejecutar
