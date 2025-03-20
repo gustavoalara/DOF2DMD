2b08fb7ab2c0a40bd41c6a744b3ce81381adf46e
gustavoalara <gustavoa.laraheredero@gmail.com>
2025-03-20 23:56:42 +0100

    Update Program.cs

--- DOF2DMD/Program.cs
+++ DOF2DMD/Program.cs
@@ -701,15 +701,11 @@ private static List<Actor> GetAllActors(object parent)
                         
                         // Arm timer once animation is done playing
 
-                        Timer animationTimer = null;
                         if (duration >= 0)
                         {
                             LogIt($"⏳AnimationTimer: Duration is great than 0, calling animation timer for {path}");
-                            animationTimer = new Timer((state) =>
-                            {
-                                AnimationTimer(state);
-                                animationTimer.Dispose(); // Eliminar el temporizador
-                            }, null, (int)(duration * 1000), Timeout.Infinite);
+                             _animationTimer?.Dispose(); 
+                            _animationTimer = new Timer(AnimationTimer, null, (int)(duration * 1000), Timeout.Infinite);
                         }
                     };
                                             
